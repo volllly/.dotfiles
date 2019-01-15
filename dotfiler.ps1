f8fdb9598aeb5f79195c57b7d86613c3e700b95c
Paul Volavsek <paul.volavsek@gmail.com>
2019-01-15 18:42:19 +0100

    update miltiline endings. add installing command.

--- dotfiler.ps1
+++ dotfiler.ps1
@@ -13,40 +13,98 @@ if (!(Get-Module -ListAvailable -Name powershell-yaml)) {
 } 
 Import-Module powershell-yaml
 
+$cfg = @{}
+
 Get-ChildItem $PSScriptRoot -Directory | ForEach-Object {
   $currentDirectory = $_
   $currentName = Split-Path $currentDirectory -Leaf
   $cfgfile = ''
-  foreach ($line in Get-Content $(Join-Path -Path $currentDirectory -ChildPath "windows.yaml")) { $cfgfile += "`n" + $line }
-  $cfg = ConvertFrom-YAML $cfgfile
+  $currentFile = $(Join-Path -Path $currentDirectory -ChildPath "windows.yaml")
+
+  if(!(Test-Path $currentFile)) {
+    Return
+  }
+  
+  foreach ($line in Get-Content $currentFile) { $cfgfile += "`n" + $line }
+  $cfg[$currentName] = ConvertFrom-YAML $cfgfile
+  if($cfg[$currentName]["installs"]) {
+    if($cfg[$currentName]["installs"].GetType().Name -eq "String") {
+      $cfg[$currentName]["installs"] = @{ "cmd" = $cfg[$currentName]["installs"]}
+    }
+    if($cfg[$currentName]["installs"]["depends"]) {
+      if($cfg[$currentName]["installs"]["depends"].GetType().Name -eq "String") {
+        $cfg[$currentName]["installs"]["depends"] = @($cfg[$currentName]["installs"]["depends"])
+      }
+    }
+    $cfg[$currentName]["installs"]["installed"] = $False
+  }
+}
+
+
+function Installs($name) {
+  $installs = $cfg[$name]["installs"]
+  $depends = $cfg[$name]["depends"]
+
+  if(($installs["installed"]) -Or ($installs["error"])) {
+    Return
+  }
+
+  if(($installs["depended"] -Gt 1) -Or ($cfg[$name]["depended"] -Gt 1)) {
+    Write-Host "detected circular dependency for $name" -ForegroundColor Red
+    Return
+  }
+  if($installs["depends"]) {
+    $installs["depends"] | ForEach-Object {
+    $cfg[$_]["installs"]["depended"] += 1
+    Installs $_
+    }
+  }
+
+  Write-Host "installing $name"
+
+  $cfg[$name]["installs"]["installed"] = $True
+
+  Invoke-Expression $installs["cmd"]
+
+  if($depends) {
+    $depends | ForEach-Object {
+      $cfg[$_]["depended"] += 1
+      Installs $_
+    }
+  }
+
+  Write-Host ""
+}
+
+function Links($name) {
+  Write-Host "linking $name"
+  $links = $cfg[$name]["links"]
+  $links.Keys | ForEach-Object {
+    Write-Host "  $_ -> $($links[$_])"
+    New-Item -Path $links.$_ -ItemType HardLink -Value $(Join-Path -Path $PSScriptRoot -ChildPath $(Join-Path -Path $name -ChildPath $_)) -Force | Out-Null
+  }
+  Write-Host ""
+}
+
+function Upadtes($name) {
+
+  Write-Host ""
+}
+
+$cfg.Keys | ForEach-Object {
+  $name = $_
   Switch($command) {
     "install" {
-      if($cfg["installs"]) {
-        Write-Output "installing $($currentName)"
-        if($cfg["installs"].GetType().Name -eq "String") {
-          $cfg["installs"] = @{ "cmd" = $cfg["installs"]}
-        }
-        if($cfg["installs"]["depends"]) {
-          if($cfg["installs"]["depends"].GetType().Name -eq "String") {
-            $cfg["installs"]["depends"] = @($cfg["installs"]["depends"])
-          }
-        }
+      if($cfg[$name].ContainsKey("installs")) {
+        Installs $name
       }
     }
     "update" {
 
     }
     "link" {
-      if($cfg["links"]) {
-        $write = $True
-        $cfg["links"].Keys | ForEach-Object {
-          if($write) { 
-            Write-Output "linking $($currentName)"
-            $write = $False
-          }
-          Write-Output "  $($_) -> $($cfg["links"].$_)"
-          New-Item -Path $cfg["links"].$_ -ItemType HardLink -Value $(Join-Path -Path $currentDirectory -ChildPath $_) -Force | Out-Null
-        }
+      if($cfg[$name]["links"]) {
+        Links $name
       }
     }
   }
