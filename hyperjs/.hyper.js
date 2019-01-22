b0f270ae13b07c2f11ce56ede442084877cc5d13
Paul Volavsek <paul.volavsek@gmail.com>
2019-01-22 12:16:04 +0100

    fix errors.

--- hyperjs/.hyper.js
+++ hyperjs/.hyper.js
@@ -101,7 +101,7 @@ module.exports = {
 
     // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
     // by default `['--login']` will be used
-    shellArgs: ['-i'],
+    shellArgs: [],
 
     // for environment variables
     env: {},
