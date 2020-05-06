5f5ebd151d0ffac34893bee4bc36feeba9ff6c57
Paul Volavsek <paul.volavsek@fiskaltrust.at>
2020-05-06 22:05:35 +0200

    update.

--- pwsh/Profile.ps1
+++ pwsh/Profile.ps1
@@ -1,6 +1,3 @@
-$env:PYTHONIOENCODING="utf-8"
-Invoke-Expression "$(thefuck --alias)"
-
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineOption -ShowToolTips
 
@@ -13,29 +10,5 @@ $importScripts | ForEach-Object {
     Set-Alias -Name $(Get-Item $_).BaseName -Value $_
   }
 }
-
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-(& "C:\Users\paul.volavsek\scoop\apps\anaconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
-#endregion
-
-# function prompt {
-#   $path = $(Get-Location).Path
-#   if($path.StartsWith($HOME)) {
-#     $path = "~$($path.Substring($HOME.Length))"
-#   }
-#   $prompt = ""
-#   $split = $path.Split("\")
-#   $index = 0
-#   foreach ($segment in $split) {
-#     $index += 1
-#     if(!($index -eq $split.Length)) {
-#       $prompt += "$($segment.Substring(0, 1))\"
-#     } else {
-#       $prompt += $segment
-#     }
-#   }
-#   "$prompt> "
-# }
 
 Invoke-Expression (&starship init powershell)
