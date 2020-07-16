69028a95ac0db78289fbabd16320163bead7d334
Paul Volavsek <paul.volavsek@fiskaltrust.at>
2020-07-16 13:44:04 +0200

    update powershell.

--- pwsh/Profile.ps1
+++ pwsh/Profile.ps1
@@ -2,3 +2,17 @@ Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineOption -ShowToolTips
 
 Invoke-Expression (&starship init powershell)
+
+function d() {
+  param(
+    [Parameter(Position = 0)]
+    [String]
+    $factor,
+    [Parameter(ValueFromRemainingArguments)]
+    [String[]]
+    $arguments
+  )
+  foreach($argument in $arguments) {
+    iex "$factor $argument"
+  }
+}
