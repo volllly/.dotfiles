5bc8ead8f9a7e761df0986f72911028a81bf2d8f
Paul Volavsek <paul.volavsek@gmail.com>
2020-09-10 10:27:28 +0200

    update vscode. update pwsh.

--- pwsh/Profile.ps1
+++ pwsh/Profile.ps1
@@ -16,3 +16,48 @@ function d() {
     iex "$factor $argument"
   }
 }
+
+
+# function d() {
+#   param(
+#     [Parameter(ValueFromRemainingArguments)]
+#     [String[]]
+#     $arguments,
+#     $opening = '[',
+#     $closing = ']'
+#   )
+#   Write-Host $arguments
+
+#   if(!($arguments | Foreach-Object { $open = 0; $close = 0} {
+#     if($_ -eq '[') {
+#       $open += 1
+#     }
+#     if($_ -eq ']') {
+#       $close += 1
+#     }
+#   } { $open -eq $close })) { Write-Error 'Differient number of opening and closing brackets.'; return }
+  
+
+#   function y($arguments) {
+#     $
+#     if($args.contains($opening)) {
+#       $factor = $arguments[0..$args.indexOf($opening)] -join ' '
+#     } else {
+#       return $arguments -join ' '
+#     }
+    
+
+#   }
+
+#   $results = y($arguments)
+
+#   $arguments.IndexOf($opening)
+#   foreach($result in $results) {
+#     Invoke-Expression $result
+#   }
+# }
+
+# a b c                 -> a b c
+# a [b c d]             -> ab ac ad
+# a [b [c d e] f g]     -> a [bc bd be f g]     -> abc abd be af ag
+# a [b [c [d e f] g] h] -> a [b [cd ce cf g] h] -> a [bcd bce bcf bg h] -> abcd abce abcf abg ah
