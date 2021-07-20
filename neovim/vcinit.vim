c9742d510c401c039951f68fb10ceb9dd578ab95
Paul Volavsek <paul.volavsek@fiskaltrust.at>
2021-07-20 13:57:38 +0200

    update

---
+++ neovim/vcinit.vim
@@ -0,0 +1,3 @@
+" editor config
+set tabstop=2 softtabstop=2 expandtab shiftwidth=2 smarttab
+set clipboard^=unnamed,unnamedplus " use system clipboard
--- pwsh/Profile.ps1
+++ pwsh/Profile.ps1
@@ -17,6 +17,10 @@ function d() {
   }
 }
 
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell) -join "`n"
+});
 
 # function d() {
 #   param(
