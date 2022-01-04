a4f0763a2ef98e13117b188e732929042f92e9e1
Paul Volavsek <paul.volavsek@fiskaltrust.at>
2022-01-04 09:20:01 +0100

    u

--- pwsh/Profile.ps1
+++ pwsh/Profile.ps1
@@ -5,7 +5,7 @@ Invoke-Expression (&starship init powershell)
 
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell) -join "`n"
-});
+    (zoxide init --hook $hook powershell | Out-String)
+})
 
 Remove-Alias sp -Force
